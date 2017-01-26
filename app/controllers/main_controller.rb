d676aee848e15c588bee7e45d0903164f5f72d60
Fabrizio <fabri.ber87@gmail.com>
2017-01-26 14:53:00 +0100

    Initial Commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,7 @@
+class MainController < ApplicationController
+
+	layout 'application'
+
+	def welcome
+	end
+end
---
+++ config/initializers/assets.rb
@@ -0,0 +1,23 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( default.css )
+
+Rails.application.config.assets.precompile += %w( jquery-1.10.2.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( jquery.easing.min.js )
+Rails.application.config.assets.precompile += %w( jquery.scrollTo.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+Rails.application.config.assets.precompile += %w( custom.js )
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
