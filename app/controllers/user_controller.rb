406ead091f7ddff95e7b75ebe0853851a859c39f
Fabrizio <fabri.ber87@gmail.com>
2017-01-30 19:04:10 +0100

    Before Removing all Javascript Files

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,10 @@ class UserController < ApplicationController
 
 	def welcome
 	end
+
+	def login
+	end
+
+	def signup
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   #get '/welcome' => 'main#welcome'
   get '/welcome' => 'user#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '/:controller(/:action(/:id))', :via => :get
 end
