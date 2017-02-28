75b842022123ce5e999c2b355dac05d6931b348b
Fabrizio <fabri.ber87@gmail.com>
2017-02-28 14:40:06 +0100

    facebook url

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,7 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], callback_url: "http://127.0.0.1:3000/users/auth/facebook/callback", scope: 'public_profile, email', image_size: 'large', provider_ignores_state: true #[:width => 34, :height => 34]
+  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], callback_url: " https://barteringapps.herokuapp.com/users/auth/facebook/callback", scope: 'public_profile, email', image_size: 'large', provider_ignores_state: true #[:width => 34, :height => 34]
  # OmniAuth.config.full_host = "http://127.0.0.1:3000" # Or have an environment specific URL.
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
