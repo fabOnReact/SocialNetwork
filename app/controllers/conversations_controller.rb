818bac5c29772dfdf65d53775e5ecc40d42c9bbc
Fabrizio <fabri.ber87@gmail.com>
2017-03-04 10:46:52 +0100

    I edited the messages pages

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,6 @@
 class ConversationsController < ApplicationController
-=begin
-	before_action :authenticate_user
+
+	before_action :authenticate_user!
 
 	def index
 		@users = User.all
@@ -11,5 +11,15 @@ class ConversationsController < ApplicationController
 		if Conversation.between(params[:sender_id],params[:recipient_id]).present?
 			@conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
 		else
-=end
+			@conversation = Conversation.create!(conversation_params)
+		end
+
+		redirect_to conversation_messages_path(@conversation)
+	end
+
+	private
+	def conversation_params
+		params.permit(:sender_id, :recipient_id)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  resources :location_images
 # omniauth facebook
   devise_for :users, :controllers => { 
   	:omniauth_callbacks => "users/omniauth_callbacks",
@@ -16,30 +15,12 @@ Rails.application.routes.draw do
   get '/tasks/:id/delete' => 'tasks#delete', :as => :delete_task
   get '/location_image/:id/delete' => 'location_images#delete', :as => :delete_location_images
 
-  resources :developers, :hosts, :projects, :locations, :ads, :tasks
+  resources :developers, :hosts, :projects, :locations, :ads, :tasks, :location_images
+  resources :conversations do
+    resources :messages
+  end
 
   match 'user_root' => 'user#welcome', as: :user_root, :via => :get
   
   root :to => 'main#welcome'
 end
-
-
-
-
-
-
-
-
-
-
-
-=begin
-  do
-  get '/users/connect/:network', :to => redirect("/users/auth/%{network}"), :as => 'user_oauth_connect'
-  end
-
- log out
- devise_scope :user do
-  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
- end  
-=end
