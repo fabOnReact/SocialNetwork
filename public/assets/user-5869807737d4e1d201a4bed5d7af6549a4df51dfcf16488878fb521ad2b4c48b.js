e0979cdb4f66c01d0550dacaf4bcc25b0c7aba44
Fabrizio <fabri.ber87@gmail.com>
2017-03-17 15:21:03 +0100

    delete public/assets on git and local

--- public/assets/user-5869807737d4e1d201a4bed5d7af6549a4df51dfcf16488878fb521ad2b4c48b.js
+++
@@ -1,7 +0,0 @@
-$(document).on('ready page:load', function() {
-    $('#buttonSearch').click(function (event) {
-        event.preventDefault()
-        $('#advancedSearch').slideToggle("slow");
-    });
-
-});
