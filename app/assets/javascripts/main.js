bca46db67dd3ced187bf699cc0423ed7eea816c0
Fabrizio <fabri.ber87@gmail.com>
2017-03-17 20:49:39 +0100

    before removing all assets

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,5 @@
 $(document).on('ready page:load', function() {
 	$('#signupFacebookButton').click(function(){
-		$(this).effect("bounce", "slow");
-		console.log("It was called");
+		$('#signupFacebookButton').fadeOut("slow");
 	});
 });
